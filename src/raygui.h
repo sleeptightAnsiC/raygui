2ac11eb478075f9274ff719bbd1e8e08b127b74d
sleeptightAnsiC <91839286+sleeptightAnsiC@users.noreply.github.com>
2024-08-09 00:07:51 +0200

    relative include

--- src/raygui.h
+++ src/raygui.h
@@ -337,7 +337,7 @@
 #define RAYGUI_VERSION  "4.5-dev"
 
 #if !defined(RAYGUI_STANDALONE)
-    #include "raylib.h"
+    #include "../../raylib/src/raylib.h"
 #endif
 
 // Function specifiers in case library is build/used as a shared library (Windows)
